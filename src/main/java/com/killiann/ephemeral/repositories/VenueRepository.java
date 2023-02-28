0cfa73afda58049ba43d9746ddb48dd3b6deb94f
KilliannH <killiannhervagault@gmail.com>
2023-02-28 21:44:16 +0100

    testing api endpoints

--- src/main/java/com/killiann/ephemeral/repositories/VenueRepository.java
+++ src/main/java/com/killiann/ephemeral/repositories/VenueRepository.java
@@ -1,6 +1,5 @@
 package com.killiann.ephemeral.repositories;
 
-import com.killiann.ephemeral.models.Location;
 import com.killiann.ephemeral.models.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
